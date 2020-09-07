c94dd2bce0cee09e1fedfd333233b99a5a515352
Leonid <leonidb93@gmail.com>
2020-09-07 04:01:03 +0200

    implement basic snapping, add inventory, add helmet snap zone

---
+++ Assets/_Scripts/FollowToTheSide.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowToTheSide : MonoBehaviour
+{
+    public Transform Target;
+    public Vector3 Offset;
+
+    private void FixedUpdate()
+    {
+        transform.position = Target.position + Vector3.up * Offset.y
+            + Vector3.ProjectOnPlane(Target.right, Vector3.up).normalized * Offset.x
+            + Vector3.ProjectOnPlane(Target.forward, Vector3.up).normalized * Offset.z;
+
+        transform.eulerAngles = new Vector3(0, Target.eulerAngles.y, 0);
+    }
+}
